f42f6cdc8c658d4dd3ff5afdbdb81201c257f4bc
NickG365 <nick@stormjive.com>
2014-01-05 17:36:10 -0500

    Add pages for modes and services.

---
+++ modes.php
@@ -0,0 +1,23 @@
+<?php
+	require("lib/Parsedown.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SpigotMC IRC Modes and Extbans</title>
+        <style>
+            body { text-align: center; padding: 90px; }
+            h1 { font-size: 50px; }
+            body { font: 20px Helvetica, sans-serif; color: #333; }
+            article { display: block; text-align: left; width: 750px; margin: 0 auto; }
+            a { color: #dc8100; text-decoration: none; }
+            a:hover { color: #333; text-decoration: none; }
+        </style>
+    </head>
+
+    <body>
+        <article>
+	       <?php echo Parsedown::instance()->parse(file_get_contents("modes.md")); ?>
+        </article>
+    </body>
+</html>
