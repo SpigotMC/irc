15e0ba46b89ba2bc4b1198265491cbf0af6143b7
NickG365 <nick@stormjive.com>
2014-01-06 14:03:34 -0500

    Move remaining CSS to external file.

--- rules.php
+++ rules.php
@@ -5,14 +5,7 @@
 <html>
     <head>
         <title>SpigotMC IRC Rules</title>
-        <style>
-            body { text-align: center; padding: 90px; }
-            h1 { font-size: 50px; }
-            body { font: 20px Helvetica, sans-serif; color: #333; }
-            article { display: block; text-align: left; width: 750px; margin: 0 auto; }
-            a { color: #dc8100; text-decoration: none; }
-            a:hover { color: #333; text-decoration: none; }
-        </style>
+        <link href="style.css" media="all" rel="stylesheet" type="text/css" />
         <link href="markdown.css" media="all" rel="stylesheet" type="text/css" />
     </head>
 
