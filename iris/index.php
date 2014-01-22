2b45bc50833dac99b3d0b7df39855d79a378077b
NickG365 <nick@stormjive.com>
2014-01-21 20:08:32 -0500

    Remove unnecessary variable usage.

--- iris/index.php
+++ iris/index.php
@@ -7,4 +7,4 @@ if( $host == 'comstock.spi.gt' )
     $host = $servers[ rand( 0, 5 ) ] . '.spi.gt';
 }
 
-header( 'Location: ' . ( empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . $host . $_SERVER[ 'REQUEST_URI' ], true, 302 );
+header( 'Location: ' . ( empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . $host . '/iris/', true, 302 );
