3593077183b463b1c2fe4b4c01b6ff0936afafcb
GunfighterJ <joseph.jenniges@gmail.com>
2014-01-21 19:22:21 -0600

    Bah, queue xkcd for the horrible commit messages. Fix page location

--- iris/index.php
+++ iris/index.php
@@ -7,6 +7,6 @@ if( $host == 'comstock.spi.gt' )
     $host = $servers[ rand( 0, count($servers) - 1 ) ] . '.spi.gt';
 }
 
-$location = ( empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . $host . '/iris/' . htmlspecialchars($_SERVER[ 'REQUEST_URI' ]) ;
+$location = (empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . $host . htmlspecialchars($_SERVER[ 'REQUEST_URI' ]);
 
 header( "Location: $location", true, 302 );
