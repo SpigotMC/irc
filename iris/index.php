76ddea1b2e1f76b97238b52c80443e9145a3de13
NickG365 <nick@stormjive.com>
2014-07-19 19:36:34 -0400

    Revert "(Temp) Take Elmer out of webchat rotation."
    This reverts commit 83ee30e10c495e41e256ec554f19b08ad3a03c3e.

--- iris/index.php
+++ iris/index.php
@@ -1,12 +1,3 @@
 <?php
-$servers = array( 'buzz', 'toast', 'fry', 'dew', 'snow' );
-$host = gethostname();
 
-if( $host == 'elmer.spi.gt' )
-{
-    $host = $servers[ rand( 0, count($servers) - 1 ) ] . '.spi.gt';
-}
-
-$location = (empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . $host . $_SERVER[ 'REQUEST_URI' ];
-
-header( "Location: $location", true, 302 );
+header( 'Location: ' . ( empty( $_SERVER[ 'HTTPS' ] ) ? 'http://' : 'https://' ) . gethostname() . $_SERVER[ 'REQUEST_URI' ], true, 302 );
